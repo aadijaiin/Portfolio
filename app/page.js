e62a4052c629d2d7e7f66175dd1a716ac9c83afb
Aadijain123 <aadijain363@gmail.com>
2024-11-24 21:03:32 +0530

    responsive design

--- app/page.js
+++ app/page.js
@@ -11,27 +11,30 @@ import Navbar from "./components/Navbar";
 const Page = () => {
   return (
     <div className="overflow-x-hidden">
-      <Navbar />
-      {/* Section 1: Intro */}
-      <section id="Home" className="h-screen">
-        <BackgroundLines
-          className="p-8 flex flex-col justify-center items-center"
-          svgOptions={{ stroke: "blue", strokeWidth: 5 }}
-        >
-          <div className="text-center mb-8">
-            <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-              Hello World!, I am
-            </h1>
-            <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-neutral-50 to-neutral-400 w-full mt-4 drop-shadow-lg">
-              <FlipWords words={["Aadi Jain", "A Developer"]} duration={2000} />
-            </h1>
-            <p className="text-lg text-gray-300 mt-4 max-w-2xl mx-auto">
-              From concept to code, I create high-performance web apps with React
-              and Next.js. Check out my portfolio!
-            </p>
-          </div>
-        </BackgroundLines>
-      </section>
+  <Navbar />
+  {/* Section 1: Intro */}
+  <section id="Home" className="h-screen flex items-center justify-center bg-black sm:bg-transparent">
+    <BackgroundLines
+      className="p-8 flex flex-col justify-center items-center"
+      svgOptions={{ stroke: "blue", strokeWidth: 5 }}
+    >
+      <div className="text-center mb-8">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Hello World!, I am
+        </h1>
+        <h1 className="text-3xl sm:text-4xl md:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-neutral-50 to-neutral-400 w-full mt-4 drop-shadow-lg">
+          <FlipWords words={["Aadi Jain", "A Developer"]} duration={2000} />
+        </h1>
+        <p className="text-lg sm:text-xl md:text-2xl text-gray-300 mt-4 max-w-xl sm:max-w-2xl mx-auto">
+          From concept to code, I create high-performance web apps with React and Next.js. Check out my portfolio!
+        </p>
+      </div>
+    </BackgroundLines>
+  </section>
+
+
+
+
       
       {/* Section 2: About */}
       <section id="About" >
